ce5c56a1cb7c9e96ff8f5e6e540b7accb7bccbd0
marco.freire <marco.freire@ens-rennes.fr>
2018-03-16 08:34:29 +0100

    COUCOUCOUCOCULCOUCOUCOCUCOULCOU

--- marco.R
+++ marco.R
@@ -1,4 +1,4 @@
-setwd("/home/Documents/Cours/sem2/Stat/TPs/Eval/urban-funicular")
+setwd("~/marco/Documents/Cours/sem2/Stat/TPs/Eval/urban-funicular")
 tab <- read.table("programs.txt", header=TRUE, sep= "\t")
 tab_lines = tab[order(tab$LinesOfCode, decreasing = TRUE),]
 tab_dupl = tab[order(tab$DuplicatedBlocks, decreasing = TRUE),]
@@ -12,18 +12,18 @@ op = par(mar = c(9, 4, 4, 2) + 0.1)
 yticks = seq()
 barplot(table(tab$Domain), main = "Number of programs for each scientific domain", las = 2)
 
+op = par(mar = c(7, 4, 4, 2) + 0.1)
 barplot(tab_lines$LinesOfCode, names.arg = tab_lines$Code, las = 2, main = "Number of lines of code for each program")
 
+op = par(mar = c(7, 4, 4, 2) + 0.1)
 barplot(tab_dupl$DuplicatedBlocks, names.arg = tab_dupl$Code, las = 2, main = "Number of duplicated code blocks for each program")
 
-temp_noNA_dupl = tab_lines$DuplicatedLines[is.na(tab_lines$DuplicatedLines) == FALSE]
-temp_noNA_lines = tab_lines$LinesOfCode[is.na(tab_lines$DuplicatedLines) == FALSE]
-temp_noNA_code = tab_lines$Code[is.na(tab_lines$DuplicatedLines) == FALSE]
-
-barplot(temp_noNA_dupl / temp_noNA_lines, names.arg = temp_noNA_code, las = 2, main = "Number of duplicated lines of code per line of code for each program")
+data = data.frame(temp_noNA_code, temp_noNA_dupl / temp_noNA_lines)
+colnames(data) = c("Program", "DupLin_per")
+data = data[order(data$DupLin_per, decreasing = TRUE),]
+op = par(mar = c(7, 4, 4, 2) + 0.1)
+barplot(data$DupLin_per, names.arg = data$Program, las = 2, main = "Number of duplicated lines of code per line of code for each program")
 
 # Reset graphic parameters
 op = par(mar = c(5, 4, 4, 2) + 0.1)
 
-data = data.frame(temp_noNA_dupl / temp_noNA_lines)
-colnames(data) = temp_noNA_code
