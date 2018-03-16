e4e9ff33f1c5fd7f3d9462615b0ad0cb5eb1180c
marco.freire <marco.freire@ens-rennes.fr>
2018-03-16 09:53:04 +0100

    Question 5 faite, sujet et TODO list updated

--- marco.R
+++ marco.R
@@ -2,9 +2,14 @@ setwd("~/marco/Documents/Cours/sem2/Stat/TPs/Eval/urban-funicular")
 tab <- read.table("programs.txt", header=TRUE, sep= "\t")
 tab_lines = tab[order(tab$LinesOfCode, decreasing = TRUE),]
 tab_dupl = tab[order(tab$DuplicatedBlocks, decreasing = TRUE),]
-tab_C = tab[tab$Language %in% c("C", "C++", "C/C++"),]
+tab_C = tab[tab$Language == "C",]
+tab_CO = tab[tab$Language %in% c("C++", "C/C++"),]
 
-#tab$assert: number of assert() for 1000 lines of code
+tab_short = tab[tab$LinesOfCode <= median(tab$LinesOfCode),]
+tab_long = tab[tab$LinesOfCode > median(tab$LinesOfCode),]
+
+# QUESTION 1
+# tab$assert: number of assert() for 1000 lines of code
 ######################################################
 
 barplot(table(tab$Language), main = "Number of programs written in a given language")
@@ -26,7 +31,7 @@ barplot(tab_lines$LinesOfCode, names.arg = tab_lines$Code, las = 2, main = "Numb
 op = par(mar = c(7, 4, 4, 2) + 0.1)
 barplot(tab_dupl$DuplicatedBlocks, names.arg = tab_dupl$Code, las = 2, main = "Number of duplicated code blocks for each program")
 
-#QUESTION 2
+# QUESTION 2
 ######################################################
 
 temp_noNA_dupl = tab_lines$DuplicatedLines[is.na(tab_lines$DuplicatedLines) == FALSE]
@@ -40,15 +45,24 @@ data = data[order(data$DupLin_per, decreasing = TRUE),]
 op = par(mar = c(7, 4, 4, 2) + 0.1)
 barplot(data$DupLin_per, names.arg = data$Program, las = 2, main = "Number of duplicated lines of code per line of code for each program")
 
-#QUESTION 4
+# QUESTION 4
 ######################################################
 
-temp_noNA_dupl = tab_C$DuplicatedLines[is.na(tab_C$DuplicatedLines) == FALSE]
-temp_noNA_lines = tab_C$LinesOfCode[is.na(tab_C$DuplicatedLines) == FALSE]
+temp_noNA_dupl_C = tab_C$DuplicatedLines[is.na(tab_C$DuplicatedLines) == FALSE]
+temp_noNA_lines_C = tab_C$LinesOfCode[is.na(tab_C$DuplicatedLines) == FALSE]
 
-dupLin_per = temp_noNA_dupl / temp_noNA_lines
+temp_noNA_dupl_CO = tab_CO$DuplicatedLines[is.na(tab_CO$DuplicatedLines) == FALSE]
+temp_noNA_lines_CO = tab_CO$LinesOfCode[is.na(tab_CO$DuplicatedLines) == FALSE]
 
-t.test(dupLin_per, conf.level = 0.9)
+dupLin_per_C = temp_noNA_dupl_C / temp_noNA_lines_C
+dupLin_per_CO = temp_noNA_dupl_CO / temp_noNA_lines_CO
+
+t.test(dupLin_per_C, conf.level = 0.9)
+t.test(dupLin_per_CO, conf.level = 0.9)
+
+# QUESTION 5
+######################################################
+
 
 # Reset graphic parameters
 op = par(mar = c(5, 4, 4, 2) + 0.1)
